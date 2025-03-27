de0206bbfaa64b932f12cc39fa5ed08dbbe8e536
oaherrem <olealexanderherrem@gmail.com>
2025-03-27 13:48:09 +0100

    lagt til user-agent

--- proj_environment-main/src/metar_writer.py
+++ proj_environment-main/src/metar_writer.py
@@ -9,6 +9,18 @@ def get_metar(icao_code, date):
     
     metar_url = f"{base_url}?icao={icao_code}&content_type=text/plain&content=metar&date={date}"
     
+    if "Ole" in os.getcwd():
+        headers = {
+            "User-Agent" : "NTNUProject/1.0 oaherrem@ntnu.no"
+        }
+    elif "hanna" in os.getcwd():
+        headers = {
+            "User-Agent" : "NTNUProject/1.0 hannavp@ntnu.no"
+        }
+    else:
+        print("Not valid user running function...")
+        exit(1)
+
     metar_response = requests.get(metar_url)
     
     metar_data = metar_response.text

c505c9d705fff24250ddddec996462dbd5fd120d
oaherrem <olealexanderherrem@gmail.com>
2025-03-27 22:43:11 +0100

    lagt til filter som fjerner tomme "" verdier i dataFrame_metar.py og oppdatert notebooks Oppgave_2 og Oppgave_3. har også generert 30 dager med data. Forøsker nå å generere 1 år med data for begge flyplassene Ørland og Bodø. -Ole

--- proj_environment-main/src/dataFrame_metar.py
+++ proj_environment-main/src/dataFrame_metar.py
@@ -13,79 +13,78 @@ def metar_df(interresting_variables,data_m):
     df = pd.DataFrame(columns=column_names)
     
     for datapoint in data_m["metar"]:
-        
-        row_data = {}
-        parts = datapoint.split()
-        
-        if len(parts)> 1 and "Airport" in interresting_variables:
-            row_data["Airport"] = parts[0]
+       for parts in datapoint:
+            partsList = parts.split()
+            #print(partsList)
+            row_data = {}
+            
+            if len(parts)> 1 and "Airport" in interresting_variables:
+                row_data["Airport"] = partsList[0]
 
-        if len(parts)> 2 and "Date/time" in interresting_variables:
-            row_data["Date/time"] = parts[1]
+            if len(parts)> 2 and "Date/time" in interresting_variables:
+                row_data["Date/time"] = partsList[1]
 
-        if len(parts)> 3 and "Wind/direction" in interresting_variables:
-            row_data["Wind/direction"] = parts[2] 
+            if len(parts)> 3 and "Wind/direction" in interresting_variables:
+                row_data["Wind/direction"] = partsList[2] 
 
-        if "Variable wind" in interresting_variables:
-            for part in parts:
-                if "V" in part and len(part)==7:
-                    row_data["Variable wind"] = part
-                    break
-            else: row_data["Variable wind"] = np.nan
+            if "Variable wind" in interresting_variables:
+                for part in partsList:
+                    if "V" in part and len(part)==7:
+                        row_data["Variable wind"] = part
+                        break
+                else: row_data["Variable wind"] = np.nan
 
-        if "Cloud 1" in interresting_variables:
-            for part in parts:
-                if part == "CAVOK":
-                    row_data["Cloud 1"] = part
-                    break
-            else: row_data["Cloud 1"] =np.nan
+            if "Cloud 1" in interresting_variables:
+                for part in partsList:
+                    if part == "CAVOK":
+                        row_data["Cloud 1"] = part
+                        break
+                else: row_data["Cloud 1"] =np.nan
 
-        if "Cloud 2" in interresting_variables:
-            for part in parts:
-                if "FEW" in part and len(part) > 4:
-                    row_data["Cloud 2"] = part
-                    break
-            else: row_data["Cloud 2"] = np.nan
+            if "Cloud 2" in interresting_variables:
+                for part in partsList:
+                    if "FEW" in part and len(part) > 4:
+                        row_data["Cloud 2"] = part
+                        break
+                else: row_data["Cloud 2"] = np.nan
 
-        if "Cloud 3" in interresting_variables:
-            for part in parts:
-                if "SCT" in part and len(part)> 4:
-                    row_data["Cloud 3"] = part
-                    break
-            else: row_data["Cloud 3"] =np.nan
+            if "Cloud 3" in interresting_variables:
+                for part in partsList:
+                    if "SCT" in part and len(part)> 4:
+                        row_data["Cloud 3"] = part
+                        break
+                else: row_data["Cloud 3"] =np.nan
 
-        if "Cloud 4" in interresting_variables:
-            for part in parts:
-                if "BKN" in part and len(part)> 4:
-                    row_data["Cloud 4"] = part
-                    break
-            else: row_data["Cloud 4"] =np.nan
+            if "Cloud 4" in interresting_variables:
+                for part in partsList:
+                    if "BKN" in part and len(part)> 4:
+                        row_data["Cloud 4"] = part
+                        break
+                else: row_data["Cloud 4"] =np.nan
 
-        if "Cloud 5" in interresting_variables:
-            for part in parts:
-                if "OVC" in part:
-                    row_data["Cloud 5"] = part
-                    break
-            else: row_data["Cloud 5"] =np.nan
+            if "Cloud 5" in interresting_variables:
+                for part in partsList:
+                    if "OVC" in part:
+                        row_data["Cloud 5"] = part
+                        break
+                else: row_data["Cloud 5"] =np.nan
 
-        if "Temp/dewpoint" in interresting_variables:
-            for part in parts:
-                if "/" in part and len(part)> 3:
-                    row_data["Temp/dewpoint"] = part
-                    break
-            else: row_data["Temp/dewpoint"] = np.nan
+            if "Temp/dewpoint" in interresting_variables:
+                for part in partsList:
+                    if "/" in part and len(part)> 3:
+                        row_data["Temp/dewpoint"] = part
+                        break
+                else: row_data["Temp/dewpoint"] = np.nan
 
-        if "QNH/pressure" in interresting_variables:
-            for part in parts:
-                if "Q" in part and len(part)> 3:
-                    row_data["QNH/pressure"] = part
-                    break
-                else: row_data["QNH/pressure"] = np.nan
+            if "QNH/pressure" in interresting_variables:
+                for part in partsList:
+                    if "Q" in part and len(part)> 3:
+                        row_data["QNH/pressure"] = part
+                        break
+                    else: row_data["QNH/pressure"] = np.nan
 
-        # Konverter row_data til en DataFrame og legg den til df ved hjelp av pd.concat
-        row_df = pd.DataFrame([row_data])
-        df = pd.concat([df, row_df], ignore_index=True)
-        
+            # Konverter row_data til en DataFrame og legg den til df ved hjelp av pd.concat
+            row_df = pd.DataFrame([row_data])
+            df = pd.concat([df, row_df], ignore_index=True)
     return df
 
-    

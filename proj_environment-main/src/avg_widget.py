e0e44d8e62d3eb2804bb5c4447c84d30fd692837
oaherrem <olealexanderherrem@gmail.com>
2025-04-28 15:34:06 +0200

    Fix - feilretting fikset avg_widget funksjon

--- proj_environment-main/src/avg_widget.py
+++ proj_environment-main/src/avg_widget.py
@@ -5,20 +5,22 @@ import ipywidgets as widgets
 import pandas as pd
 from IPython.display import clear_output
 
+airport = 'ENOL'
+#airport = 'ENBO'
 
-file_path = '../data/csv/ENOL_wind_data.csv'
-df_ENOL = pd.read_csv(file_path)
+file_path = f'../data/csv/{airport}_wind_data.csv'
+df_airport = pd.read_csv(file_path)
 
 def avg_widget(ses=2.5):
     clear_output(wait=True)
     plt.figure(figsize=(6, 4))
-    sns.kdeplot(df_ENOL['Wind_speed'], lw=2.5, fill=True, bw_adjust=ses)
+    sns.kdeplot(df_airport['Wind_speed'], lw=2.5, fill=True, bw_adjust=ses)
     
     plt.xlim(0, 60)
     plt.ylim(0.0, 0.1)
     plt.title(f'Vindhastighet - relativ sannsynlighet (KDE) med glatting: {ses}', fontsize=14)
-    plt.xlabel('Vindhastighet (m/s)')
-    plt.ylabel('Tetthet')
+    plt.xlabel('Vindhastighet (knop)')
+    plt.ylabel('Tetthet (prosent)')
     plt.grid(True)
     plt.show()
 
